7b346adec6d97e1551d61da8a2a68988ce01af3c
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-06-01 15:13:49 +0300

    Comlited the task

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,10 @@
 package com.epam.demo;
 
-import java.util.List;
+import com.epam.utils.StringUtils;
+import java.util.*;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
